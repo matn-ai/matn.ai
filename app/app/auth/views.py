a59811de8e5c09766d5ce6b89166b7f5553f8089
saman <you@example.com>
2024-06-23 20:52:19 +0330

    User email confirmation debugged

--- app/app/auth/views.py
+++ app/app/auth/views.py
@@ -1,23 +1,22 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required, \
-    current_user
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from .. import db
 from ..models import User
 from ..email import send_email
-from .forms import LoginForm, RegistrationForm, ChangePasswordForm,\
-    PasswordResetRequestForm, PasswordResetForm, ChangeEmailForm
+from .forms import LoginForm, RegistrationForm, ChangePasswordForm, PasswordResetRequestForm, PasswordResetForm, ChangeEmailForm
 
 
 @auth.before_app_request
 def before_request():
     if current_user.is_authenticated:
+        print(request.full_path)
         current_user.ping()
         if not current_user.confirmed \
                 and request.endpoint \
                 and request.blueprint != 'auth' \
                 and request.endpoint != 'static':
-            return redirect(url_for('auth.unconfirmed'))
+                    return redirect(url_for('auth.unconfirmed'))
 
 
 @auth.route('/unconfirmed')
@@ -30,12 +29,14 @@ def unconfirmed():
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    next = request.args.get('next')
+    if next is not None:
+        form.next_url.data = next
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data.lower()).first()
         if user is not None and user.verify_password(form.password.data):
-            # login_user(user, form.remember_me.data)
             login_user(user, remember=True)
-            next = request.args.get('next')
+            next = form.next_url.data
             if next is None or not next.startswith('/'):
                 next = url_for('dashboard.index')
             return redirect(next)
@@ -47,7 +48,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    flash('You have been logged out.')
+    flash('شما خارج شدید.')
     return redirect(url_for('main.index'))
 
 
@@ -64,7 +65,6 @@ def register():
         send_email(user.email, 'حساب خود را تایید کنید',
                    'auth/email/confirm', user=user, token=token)
         flash('یک ایمیل تایید حساب برای شما ارسال شده است، Inbox یا Spam ایمیل خود را چک کنید')
-        return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
 
 
@@ -72,13 +72,13 @@ def register():
 @login_required
 def confirm(token):
     if current_user.confirmed:
-        flash('Your account confirmed')
+        flash('حساب شما تایید شده است.')
         return redirect(url_for('main.index'))
     if current_user.confirm(token):
         db.session.commit()
-        flash('You have confirmed your account. Thanks!')
+        flash('حساب شما تایید شد. با تشکر!')
     else:
-        flash('The confirmation link is invalid or has expired.')
+        flash('لینک تایید نامعتبر یا منقضی شده است.')
     return redirect(url_for('main.index'))
 
 
@@ -86,9 +86,9 @@ def confirm(token):
 @login_required
 def resend_confirmation():
     token = current_user.generate_confirmation_token()
-    send_email(current_user.email, 'Confirm Your Account',
+    send_email(current_user.email, 'حساب خود را تایید کنید',
                'auth/email/confirm', user=current_user, token=token)
-    flash('A new confirmation email has been sent to you by email.')
+    flash('یک ایمیل تایید جدید برای شما ارسال شده است.')
     return redirect(url_for('main.index'))
 
 
@@ -101,10 +101,10 @@ def change_password():
             current_user.password = form.password.data
             db.session.add(current_user)
             db.session.commit()
-            flash('Your password has been updated.')
+            flash('رمز عبور شما بروز رسانی شد.')
             return redirect(url_for('main.index'))
         else:
-            flash('Invalid password.')
+            flash('رمز عبور نامعتبر است.')
     return render_template("auth/change_password.html", form=form)
 
 
@@ -117,11 +117,10 @@ def password_reset_request():
         user = User.query.filter_by(email=form.email.data.lower()).first()
         if user:
             token = user.generate_reset_token()
-            send_email(user.email, 'Reset Your Password',
+            send_email(user.email, 'بازیابی رمز عبور',
                        'auth/email/reset_password',
                        user=user, token=token)
-        flash('An email with instructions to reset your password has been '
-              'sent to you.')
+        flash('یک ایمیل با دستورالعمل‌های بازیابی رمز عبور برای شما ارسال شده است.')
         return redirect(url_for('auth.login'))
     return render_template('auth/reset_password.html', form=form)
 
@@ -134,9 +133,10 @@ def password_reset(token):
     if form.validate_on_submit():
         if User.reset_password(token, form.password.data):
             db.session.commit()
-            flash('Your password has been updated.')
+            flash('رمز عبور شما بروز رسانی شد.')
             return redirect(url_for('auth.login'))
         else:
+            flash('درخواست نامعتبر است.')
             return redirect(url_for('main.index'))
     return render_template('auth/reset_password.html', form=form)
 
@@ -149,14 +149,13 @@ def change_email_request():
         if current_user.verify_password(form.password.data):
             new_email = form.email.data.lower()
             token = current_user.generate_email_change_token(new_email)
-            send_email(new_email, 'Confirm your email address',
+            send_email(new_email, 'آدرس ایمیل خود را تایید کنید',
                        'auth/email/change_email',
                        user=current_user, token=token)
-            flash('An email with instructions to confirm your new email '
-                  'address has been sent to you.')
+            flash('یک ایمیل با دستورالعمل‌های تایید آدرس ایمیل جدید برای شما ارسال شده است.')
             return redirect(url_for('main.index'))
         else:
-            flash('Invalid email or password.')
+            flash('ایمیل یا رمزعبور نامعتبر است.')
     return render_template("auth/change_email.html", form=form)
 
 
@@ -165,7 +164,7 @@ def change_email_request():
 def change_email(token):
     if current_user.change_email(token):
         db.session.commit()
-        flash('Your email address has been updated.')
+        flash('آدرس ایمیل شما بروز رسانی شد.')
     else:
-        flash('Invalid request.')
+        flash('درخواست نامعتبر است.')
     return redirect(url_for('main.index'))
